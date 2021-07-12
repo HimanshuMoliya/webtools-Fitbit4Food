fe81c0615c9f4a74b74129928c733f151f1c4c63
Himanshu Moliya <himanshu@Himanshus-MacBook-Air.local>
2021-07-12 11:27:12 +0530

    IP

--- fitbit4food_prototype.py
+++ fitbit4food_prototype.py
@@ -476,7 +476,7 @@ def recommendation_engine_gui():
 								$("#reward_{idx}").on("click", function(e){{
 									e.preventDefault();
 										$.ajax({{
-										url: 'http://0.0.0.0:5000/reward',
+										url: 'http://10.12.59.199:5000/reward',
 										method: 'POST',
 										headers: {{
 											'Content-Type':'application/json'
@@ -488,7 +488,7 @@ def recommendation_engine_gui():
 								$("#feedback_{idx}").on("click", function(e){{
 									e.preventDefault();
 										$.ajax({{
-										url: 'http://0.0.0.0:5000/feedback',
+										url: 'http://10.12.59.199:5000/feedback',
 										method: 'POST',
 										headers: {{
 											'Content-Type':'application/json'
