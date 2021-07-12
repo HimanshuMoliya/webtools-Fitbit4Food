e0aa154997c71433d9f795a87cd8d9fcc3461d3f
Himanshu Moliya <himanshu@Himanshus-MacBook-Air.local>
2021-07-12 11:35:47 +0530

    IP

--- fitbit4food_prototype.py
+++ fitbit4food_prototype.py
@@ -476,7 +476,7 @@ def recommendation_engine_gui():
 								$("#reward_{idx}").on("click", function(e){{
 									e.preventDefault();
 										$.ajax({{
-										url: 'https://10.12.59.199:5000/reward',
+										url: 'https://share.streamlit.io/api/v1/app/himanshumoliya/webtools-fitbit4food/main/fitbit4food_prototype.py/reward',
 										method: 'POST',
 										headers: {{
 											'Content-Type':'application/json'
@@ -488,7 +488,7 @@ def recommendation_engine_gui():
 								$("#feedback_{idx}").on("click", function(e){{
 									e.preventDefault();
 										$.ajax({{
-										url: 'https://10.12.59.199:5000/feedback',
+										url: 'https://share.streamlit.io/api/v1/app/himanshumoliya/webtools-fitbit4food/main/fitbit4food_prototype.py/feedback',
 										method: 'POST',
 										headers: {{
 											'Content-Type':'application/json'
